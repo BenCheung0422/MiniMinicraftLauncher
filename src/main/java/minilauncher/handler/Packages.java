7ecbcb17a019093d88babc6fb0252053d8e791f2
BenChueng0422 <74168521+BenChueng0422@users.noreply.github.com>
2022-03-30 21:01:14 +0800

    add MainPage

--- src/main/java/minilauncher/handler/Packages.java
+++ src/main/java/minilauncher/handler/Packages.java
@@ -2,13 +2,41 @@ package minilauncher.handler;
 
 import java.util.ArrayList;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import minilauncher.core.App;
+import minilauncher.saveload.Version;
 
 public class Packages {
     public static void init() {}
-    public static ArrayList<String> packages = new ArrayList<>();
+    public static ArrayList<installPackage> packages = new ArrayList<>();
     static {
-        // App.dataDir
+        JSONArray installations = Installation.installations.has("installations")? Installation.installations.getJSONArray("installations"): new JSONArray();
+        for (int i = 0; i<installations.length(); i++) {
+            JSONObject install = installations.getJSONObject(i);
+            installPackage pack = new installPackage();
+            pack.name = install.getString("name");
+            pack.version = new Version(install.getString("version"));
+            pack.gameDir = install.getString("gameDir");
+            pack.saveDir = install.getString("saveDir");
+            packages.add(pack);
+        }
     }
-    
+    public static void addPackage(String name, Version ver, String gameDir, String saveDir) {
+        installPackage pack = new installPackage();
+        pack.name = name;
+        pack.version = ver;
+        pack.gameDir = gameDir;
+        pack.saveDir = saveDir;
+    }
+    public static class installPackage {
+        public String name;
+        public Version version;
+        public String gameDir;
+        public String saveDir;
+        public String toString() {
+            return name+" "+version.toString();
+        }
+    }
 }
--- src/main/java/minilauncher/layout/mainLayout/MainPage.java
+++ src/main/java/minilauncher/layout/mainLayout/MainPage.java
@@ -1,9 +1,13 @@
 package minilauncher.layout.mainLayout;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Font;
 
+import javax.swing.AbstractListModel;
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
@@ -11,9 +15,15 @@ import javax.swing.JSplitPane;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
 
+import minilauncher.handler.Packages;
 import minilauncher.layout.Layout;
 
 public class MainPage {
+    private static class packageDetails {
+        public static JLabel name;
+        public static JLabel version;
+        public static JButton button;
+    }
     public static void loadLayout(Layout layout) {
         JLabel label = new JLabel("MiniLauncher");
         label.setFont(new Font("Serif", Font.BOLD, 40));
@@ -23,16 +33,46 @@ public class MainPage {
         label.setBorder(BorderFactory.createLineBorder(Color.BLACK));;
         label.setHorizontalAlignment(SwingConstants.CENTER);
         label.setVerticalAlignment(SwingConstants.CENTER);
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
         JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.add(label);
         JList<String> list = new JList<String>();
+        list.setModel(new AbstractListModel<String>() {
+            public int getSize() { return Packages.packages.size(); }
+            public String getElementAt(int index) { return Packages.packages.get(index).toString(); }
+        });
+        list.addListSelectionListener(e -> {
+            setPackageDetail(list.getSelectedIndex());
+        });
         list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
         list.setLayoutOrientation(JList.HORIZONTAL_WRAP);
-        JSplitPane subPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        JPanel pack = new JPanel();
+        pack.setLayout(new BoxLayout(pack, BoxLayout.Y_AXIS));
+        packageDetails.name = new JLabel();
+        packageDetails.name.setFont(new Font("Serif", Font.PLAIN, 25));
+        packageDetails.name.setAlignmentX(Component.CENTER_ALIGNMENT);
+        packageDetails.name.setHorizontalAlignment(SwingConstants.CENTER);
+        packageDetails.name.setVerticalAlignment(SwingConstants.CENTER);
+        pack.add(packageDetails.name);
+        packageDetails.version = new JLabel();
+        packageDetails.version.setFont(new Font("Serif", Font.PLAIN, 18));
+        packageDetails.version.setAlignmentX(Component.CENTER_ALIGNMENT);
+        packageDetails.version.setHorizontalAlignment(SwingConstants.CENTER);
+        packageDetails.version.setVerticalAlignment(SwingConstants.CENTER);
+        pack.add(packageDetails.version);
+        JButton startButton = new JButton();
+        JSplitPane subPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, list, pack);
         subPanel.setDividerLocation(300);
+        subPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.add(subPanel);
+        panel.setBackground(new Color(15, 15, 15));
         Loading.removeLayout();
+        layout.getContentPane().add(panel);
         layout.getContentPane().setBackground(new Color(10, 10, 10));
-        layout.add(panel);
+        layout.validate();
+    }
+    private static void setPackageDetail(int index) {
+
     }
 }
